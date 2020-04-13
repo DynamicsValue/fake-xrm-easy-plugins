018f1346e7818f71573ffe8301adac88c36d867b
jordimontana82 <jordi.montana@gmail.com>
2020-04-13 15:52:58 +0200

    First commit

---
+++ build-push.ps1
@@ -0,0 +1,39 @@
+param (
+    [string]$packageSource = "local-packages",
+    [string]$versionSuffix = ""
+ )
+
+
+Write-Host "Running with packageSource '$($packageSource)' and versionSuffix '$($versionSuffix)'..."
+
+Write-Host "Building..."
+
+./build.ps1
+
+$tempNupkgFolder = './nupkgs'
+Write-Host "Checking if temp nupkgs folder '$($localPackagesFolder)' exists..."
+
+$tempNupkgFolderExists = Test-Path $tempNupkgFolder -PathType Container
+
+if(!($tempNupkgFolderExists)) 
+{
+    New-Item $tempNupkgFolder -ItemType Directory
+}
+
+Write-Host "Deleting temporary nupkgs..."
+Get-ChildItem -Path $tempNupkgFolder -Include *.* -File -Recurse | ForEach-Object { $_.Delete()}
+
+Write-Host "Packing assembly..."
+if($versionSuffix -eq "") 
+{
+    dotnet pack -o $tempNupkgFolder src/FakeXrmEasy.Plugins/FakeXrmEasy.Plugins.csproj
+}
+else {
+    dotnet pack -o $tempNupkgFolder src/FakeXrmEasy.Plugins/FakeXrmEasy.Plugins.csproj /p:VersionSuffix=$versionSuffix
+}
+
+
+Write-Host "Pushing FakeXrmEasy.Plugins to local folder..."
+dotnet nuget push $tempNupkgFolder/*.nupkg -s $packageSource
+
+Write-Host "Succeeded :)"
