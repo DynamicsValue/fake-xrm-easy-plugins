b87f3cec583d237386349e0a13f7a5661a6ceb6d
jordimontana82 <jordi.montana@gmail.com>
2020-12-25 21:13:44 +0100

    Updated build scripts

--- build.ps1
+++ build.ps1
@@ -1,3 +1,6 @@
+param (
+    [string]$targetFramework = "netcoreapp3.1"
+ )
 
 $localPackagesFolder = '../local-packages'
 Write-Host "Checking if local packages folder '$($localPackagesFolder)' exists..."
@@ -8,18 +11,38 @@ if(!($packagesFolderExists))
 {
     New-Item $localPackagesFolder -ItemType Directory
 }
-
-dotnet restore
+if($targetFramework -eq "all")
+{
+    dotnet restore
+}
+else {
+    dotnet restore -p:TargetFrameworks=$targetFramework
+}
 if(!($LASTEXITCODE -eq 0)) {
     throw "Error restoring packages"
 }
 
-dotnet build --configuration Debug --no-restore
+if($targetFramework -eq "all")
+{
+    dotnet build --configuration Debug --no-restore
+}
+else 
+{
+    dotnet build --configuration Debug --no-restore --framework $targetFramework
+}
 if(!($LASTEXITCODE -eq 0)) {
     throw "Error during build step"
 }
 
-dotnet test --configuration Debug --no-restore --verbosity normal --collect:"XPlat code coverage" --settings tests/.runsettings
+if($targetFramework -eq "all")
+{
+    dotnet test --configuration Debug --no-restore --verbosity normal --collect:"XPlat code coverage" --settings tests/.runsettings --results-directory ./coverage
+}
+else 
+{
+    dotnet test --configuration Debug --no-restore --framework $targetFramework --verbosity normal --collect:"XPlat code coverage" --settings tests/.runsettings --results-directory ./coverage
+}
+
 if(!($LASTEXITCODE -eq 0)) {
     throw "Error during test step"
 }
---
+++ pack.ps1
@@ -0,0 +1,47 @@
+param (
+    [string]$versionSuffix = "",
+    [string]$targetFrameworks = "netcoreapp3.1"
+ )
+
+ $project = "FakeXrmEasy.Plugins"
+
+Write-Host "Running with versionSuffix '$($versionSuffix)'..."
+
+$tempNupkgFolder = './nupkgs'
+
+Write-Host "Checking if temp nupkgs folder '$($tempNupkgFolder)' exists..."
+$tempNupkgFolderExists = Test-Path $tempNupkgFolder -PathType Container
+if(!($tempNupkgFolderExists)) 
+{
+    New-Item $tempNupkgFolder -ItemType Directory
+}
+
+Write-Host "Deleting temporary nupkgs..."
+Get-ChildItem -Path $tempNupkgFolder -Include *.nupkg -File -Recurse | ForEach-Object { $_.Delete()}
+
+Write-Host "Packing assembly for targetFrameworks $($targetFrameworks)..."
+if($targetFrameworks -eq "all")
+{
+    if($versionSuffix -eq "") 
+    {
+        dotnet pack -o $tempNupkgFolder src/$project/$project.csproj
+    }
+    else {
+        dotnet pack -o $tempNupkgFolder src/$project/$project.csproj --version-suffix $versionSuffix
+    }
+}
+else 
+{
+    if($versionSuffix -eq "") 
+    {
+        dotnet pack -p:TargetFrameworks=$targetFrameworks -o $tempNupkgFolder src/$project/$project.csproj
+    }
+    else {
+        dotnet pack -p:TargetFrameworks=$targetFrameworks -o $tempNupkgFolder src/$project/$project.csproj --version-suffix $versionSuffix
+    }
+}
+if(!($LASTEXITCODE -eq 0)) {
+    throw "Error when packing the assembly"
+}
+
+Write-Host "Pack Succeeded :)" -ForegroundColor Green
