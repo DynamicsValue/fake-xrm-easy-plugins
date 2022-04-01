194916ee1d034d2f6a3449d45c8b32ab1351ed08
Jordi <jordi.montana@gmail.com>
2022-04-01 16:55:10 +0200

    Added benchmark tests, improve performance of PluginSteps retrieval DynamicsValue/fake-xrm-easy#14

---
+++ performance/FakeXrmEasy.Plugins.Performance/PipelineBenchmarks.cs
@@ -0,0 +1,42 @@
+﻿using BenchmarkDotNet.Attributes;
+using Crm;
+using FakeXrmEasy.Abstractions.Plugins.Enums;
+using FakeXrmEasy.Pipeline;
+using FakeXrmEasy.Plugins.Performance.PluginsForTesting;
+using Microsoft.Xrm.Sdk.Messages;
+
+namespace FakeXrmEasy.Plugins.Performance
+{
+    public class PipelineBenchmarks: FakeXrmEasyBenchmarksBase
+    {
+        private readonly CreateRequest _createRequest;
+        private readonly Account _target;
+
+        public PipelineBenchmarks()
+        {
+            _target = new Account() { };
+
+            _createRequest = new CreateRequest()
+            {
+                Target = _target
+            };
+
+            for (var i = 0; i < 1000; i++)
+            {
+                _context.RegisterPluginStep<AccountNumberPlugin>("Create", ProcessingStepStage.Preoperation, ProcessingStepMode.Synchronous, rank: i + 1);
+            }
+        }
+
+        [Benchmark]
+        public void GetPluginStepsWithRetrieveMultiple()
+        {
+            _context.GetPluginStepsForOrganizationRequestWithRetrieveMultiple("Create", ProcessingStepStage.Preoperation, ProcessingStepMode.Synchronous, _createRequest);
+        }
+
+        [Benchmark]
+        public void GetPluginStepsWithoutRetrieveMultiple()
+        {
+            _context.GetPluginStepsForOrganizationRequest("Create", ProcessingStepStage.Preoperation, ProcessingStepMode.Synchronous, _createRequest);
+        }
+    }
+}
---
+++ performance/FakeXrmEasy.Plugins.Performance/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace FakeXrmEasy.Plugins.Performance
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run(typeof(PipelineBenchmarks));
+        }
+    }
+}
