1e4ef8a544edb773b40fd01786d8e236bd047ac6
Jordi <jordi.montana@gmail.com>
2022-04-08 16:18:14 +0200

    Add plugin validator to validate Upsert registrations DynamicsValue/fake-xrm-easy#19

---
+++ src/FakeXrmEasy.Plugins/PluginSteps/PluginStepValidator.cs
@@ -0,0 +1,93 @@
+﻿using FakeXrmEasy.Abstractions.Plugins.Enums;
+using System.Collections.Generic;
+
+namespace FakeXrmEasy.Plugins.PluginSteps
+{
+    /// <summary>
+    /// The plugin step validator is in charge of validating that a given combination of message, entity, stage, and mode is valid (that it can be registered in plugin registration tool)
+    /// </summary>
+    public interface IPluginStepValidator
+    {
+        /// <summary>
+        /// Returns true if the given combination is valid
+        /// </summary>
+        /// <param name="messageName"></param>
+        /// <param name="entityLogicalName"></param>
+        /// <param name="stage"></param>
+        /// <param name="mode"></param>
+        /// <returns></returns>
+        bool IsValid(string messageName, string entityLogicalName, ProcessingStepStage stage, ProcessingStepMode mode);
+    }
+
+    /// <summary>
+    /// Default implementation of IPluginStepValidator
+    /// </summary>
+    public class PluginStepValidator: IPluginStepValidator
+    {
+        private readonly Dictionary<string, Dictionary<string, Dictionary<ProcessingStepStage, Dictionary<ProcessingStepMode, int>>>> _combinations;
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public PluginStepValidator()
+        {
+            _combinations = new Dictionary<string, Dictionary<string, Dictionary<ProcessingStepStage, Dictionary<ProcessingStepMode, int>>>>()
+            {
+                { "upsert", new Dictionary<string, Dictionary<ProcessingStepStage, Dictionary<ProcessingStepMode, int>>>()
+                    {
+                        { "appnotification", new Dictionary<ProcessingStepStage, Dictionary<ProcessingStepMode, int>>()
+                            {
+                                { ProcessingStepStage.Preoperation, new Dictionary<ProcessingStepMode, int>()
+                                    {
+                                        {  ProcessingStepMode.Synchronous, 0 },
+                                        {  ProcessingStepMode.Asynchronous, 0 }
+                                    }
+                                },
+                                { ProcessingStepStage.Postoperation, new Dictionary<ProcessingStepMode, int>()
+                                    {
+                                        {  ProcessingStepMode.Synchronous, 0 },
+                                        {  ProcessingStepMode.Asynchronous, 0 }
+                                    }
+                                }
+                            }
+                        },
+                        { "searchtelemetry", new Dictionary<ProcessingStepStage, Dictionary<ProcessingStepMode, int>>()
+                            {
+                                { ProcessingStepStage.Preoperation, new Dictionary<ProcessingStepMode, int>()
+                                    {
+                                        {  ProcessingStepMode.Synchronous, 0 },
+                                        {  ProcessingStepMode.Asynchronous, 0 }
+                                    }
+                                },
+                                { ProcessingStepStage.Postoperation, new Dictionary<ProcessingStepMode, int>()
+                                    {
+                                        {  ProcessingStepMode.Synchronous, 0 },
+                                        {  ProcessingStepMode.Asynchronous, 0 }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+
+  
+        }
+        public bool IsValid(string messageName, string entityLogicalName, ProcessingStepStage stage, ProcessingStepMode mode)
+        {
+            if (!_combinations.ContainsKey(messageName.ToLower()))
+                return false;
+
+            if (!_combinations[messageName].ContainsKey(entityLogicalName.ToLower()))
+                return false;
+
+            if (!_combinations[messageName][entityLogicalName].ContainsKey(stage))
+                return false;
+
+            if (!_combinations[messageName][entityLogicalName][stage].ContainsKey(mode))
+                return false;
+
+            return true;
+        }
+    }
+}
---
+++ tests/FakeXrmEasy.Plugins.Tests/PluginSteps/PluginStepValidatorTests.cs
@@ -0,0 +1,37 @@
+﻿using FakeXrmEasy.Abstractions.Plugins.Enums;
+using FakeXrmEasy.Plugins.PluginSteps;
+using Xunit;
+
+namespace FakeXrmEasy.Plugins.Tests.PluginSteps
+{
+    public class PluginStepValidatorTests
+    {
+        private readonly PluginStepValidator _validator;
+
+        public PluginStepValidatorTests()
+        {
+            _validator = new PluginStepValidator();
+        }
+
+        [Theory]
+        [InlineData("upsert", "appnotification", ProcessingStepStage.Preoperation, ProcessingStepMode.Synchronous)]
+        [InlineData("upsert", "appnotification", ProcessingStepStage.Postoperation, ProcessingStepMode.Synchronous)]
+        [InlineData("upsert", "appnotification", ProcessingStepStage.Preoperation, ProcessingStepMode.Asynchronous)]
+        [InlineData("upsert", "appnotification", ProcessingStepStage.Postoperation, ProcessingStepMode.Asynchronous)]
+        [InlineData("upsert", "searchtelemetry", ProcessingStepStage.Preoperation, ProcessingStepMode.Synchronous)]
+        [InlineData("upsert", "searchtelemetry", ProcessingStepStage.Postoperation, ProcessingStepMode.Synchronous)]
+        [InlineData("upsert", "searchtelemetry", ProcessingStepStage.Preoperation, ProcessingStepMode.Asynchronous)]
+        [InlineData("upsert", "searchtelemetry", ProcessingStepStage.Postoperation, ProcessingStepMode.Asynchronous)]
+        public void Should_return_valid_registration_for_valid_combinations(string messageName, string entityLogicalName, ProcessingStepStage stage, ProcessingStepMode mode)
+        {
+            Assert.True(_validator.IsValid(messageName, entityLogicalName, stage, mode));
+        }
+
+        [Theory]
+        [InlineData("upsert", "*", ProcessingStepStage.Preoperation, ProcessingStepMode.Synchronous)]
+        public void Should_return_invalid_registration_for_invalid_combinations(string messageName, string entityLogicalName, ProcessingStepStage stage, ProcessingStepMode mode)
+        {
+            Assert.False(_validator.IsValid(messageName, entityLogicalName, stage, mode));
+        }
+    }
+}
