f358609ecb61d9921fd1abb82ee82acc159e7f92
Jordi <jordi.montana@gmail.com>
2022-12-17 13:26:02 +0100

    Adding several tests DynamicsValue/fake-xrm-easy#50

---
+++ tests/FakeXrmEasy.Plugins.Tests/Middleware/CustomApis/CustomApiFakeMessageExecutorTests.cs
@@ -0,0 +1,54 @@
+﻿using FakeXrmEasy.Plugins.Middleware.CustomApis;
+using Microsoft.Xrm.Sdk;
+using System;
+using Xunit;
+
+namespace FakeXrmEasy.Plugins.Tests.Middleware.CustomApis
+{
+    public class CustomApiFakeMessageExecutorTests
+    {
+        private class MyFakePlugin : IPlugin
+        {
+            public void Execute(IServiceProvider serviceProvider)
+            {
+                //Do nothing...
+            }
+        }
+
+        private class MyCustomApiRequest: OrganizationRequest
+        {
+            public static string MessageName = "FakeRequest";
+            public MyCustomApiRequest()
+            {
+                RequestName = MessageName;
+            }
+        }
+
+        private class MyGenericCustomApiWithPluginExecutor: CustomApiFakeMessageExecutor<MyFakePlugin>
+        {
+            public override string MessageName => MyCustomApiRequest.MessageName;
+        }
+
+        [Fact]
+        public void Should_set_plugin_type_and_message_name_properties_for_early_bound_custom_apis()
+        {
+            var customApi = new CustomApiFakeMessageExecutor<MyFakePlugin, MyCustomApiRequest>();
+
+            Assert.NotNull(customApi.PluginType);
+
+            Assert.IsType<MyFakePlugin>(customApi.PluginType);
+            Assert.Equal(MyCustomApiRequest.MessageName, customApi.MessageName);
+        }
+
+        [Fact]
+        public void Should_set_message_name_properties_for_generic_custom_apis()
+        {
+            var customApi = new MyGenericCustomApiWithPluginExecutor();
+
+            Assert.NotNull(customApi.PluginType);
+
+            Assert.IsType<MyFakePlugin>(customApi.PluginType);
+            Assert.Equal(MyCustomApiRequest.MessageName, customApi.MessageName);
+        }
+    }
+}
