7ac3b16fe86c241493c0297ed1f52d382f248755
Jordi <jordi.montana@gmail.com>
2023-05-17 02:11:11 +0200

    Should resolve DynamicsValue/fake-xrm-easy#102

---
+++ src/FakeXrmEasy.Plugins/Extensions/EntityExtensions.cs
@@ -0,0 +1,34 @@
+﻿
+
+using FakeXrmEasy.Extensions;
+using Microsoft.Xrm.Sdk;
+
+namespace FakeXrmEasy.Plugins.Extensions
+{
+    /// <summary>
+    /// Entity extensions
+    /// </summary>
+    public static class EntityExtensions
+    {
+        /// <summary>
+        /// Replaces existing attributes in entity1, with the values from entity2
+        /// </summary>
+        /// <param name="entity1"></param>
+        /// <param name="entity2"></param>
+        /// <returns></returns>
+        public static Entity ReplaceAttributesWith(this Entity entity1, Entity entity2)
+        {
+            var clone = entity1.Clone();
+
+            foreach(var key in entity1.Attributes.Keys)
+            {
+                if(entity2.Attributes.ContainsKey(key))
+                {
+                    clone[key] = entity2[key];
+                }
+            }
+
+            return clone;
+        }
+    }
+}
