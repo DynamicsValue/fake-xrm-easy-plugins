f1830df146f9bd0a0d42af30dcc06241be6832d6
Jordi <jordi.montana@gmail.com>
2025-05-11 13:07:46 +0200

    Add conditional compilation to generated entities to support multiple versions

--- tests/DataverseEntities.Shared/Entities/dv_test.cs
+++ tests/DataverseEntities.Shared/Entities/dv_test.cs
@@ -366,6 +366,7 @@ namespace DataverseEntities
 			}
 		}
 		
+		#if FAKE_XRM_EASY_9
 		[Microsoft.Xrm.Sdk.AttributeLogicalNameAttribute("dv_choice_multiple")]
 		public virtual System.Collections.Generic.IEnumerable<dv_test_dv_choice_multiple> dv_choice_multiple
 		{
@@ -394,6 +395,7 @@ namespace DataverseEntities
 				}
 			}
 		}
+		#endif
 		
 		[Microsoft.Xrm.Sdk.AttributeLogicalNameAttribute("dv_choicename")]
 		public string dv_choiceName
