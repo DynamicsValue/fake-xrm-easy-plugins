f91243e4f4465d18e639a1870d4b8c073cc44754
Jordi <jordi.montana@gmail.com>
2025-07-21 16:22:49 +0200

    Adds plugins for testing

---
+++ tests/FakeXrmEasy.Tests.PluginsForTesting.Shared/ConfigurationPluginPipeline.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Metadata;
+using System;
+
+namespace FakeXrmEasy.Tests
+{
+    /// <summary>
+    /// Mock plugin to test pipeline simulation with secure and unsecure configurations
+    /// </summary>
+    public class ConfigurationPluginPipeline : IPlugin
+    {
+        private readonly string _unsecureConfiguration;
+        private readonly string _secureConfiguration;
+
+        /// <summary>
+        /// Parameterless constructor
+        /// </summary>
+        public ConfigurationPluginPipeline() : this(string.Empty, string.Empty)
+        {
+        }
+
+        /// <summary>
+        /// Constructor with configurations
+        /// </summary>
+        /// <param name="unsecureConfiguration">The unsecure configuration</param>
+        /// <param name="secureConfiguration">The secure configuration</param>
+        public ConfigurationPluginPipeline(string unsecureConfiguration, string secureConfiguration)
+        {
+            _unsecureConfiguration = unsecureConfiguration;
+            _secureConfiguration = secureConfiguration;
+        }
+
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            var context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            var target = (Entity)context.InputParameters["Target"];
+            var serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            var service = serviceFactory.CreateOrganizationService(context.UserId);
+            
+            target["unsecure"] = _unsecureConfiguration;
+            target["secure"] = _secureConfiguration;
+
+            service.Create(new Entity("dummyconfigurationresponse")
+            {
+                ["unsecure"] = _unsecureConfiguration,
+                ["secure"] = _secureConfiguration
+            });
+        }
+    }
+}
---
+++ tests/FakeXrmEasy.Tests.PluginsForTesting.Shared/EntityImagesInPluginPipeline.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+
+namespace FakeXrmEasy.Plugins.Tests.PluginsForTesting
+{
+    public class EntityImagesInPluginPipeline : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            var context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            if (context == null)
+                throw new InvalidPluginExecutionException("Initialize IPluginExecutionContext fail.");
+
+            if (!context.InputParameters.Contains("Target"))
+            {
+                throw new InvalidPluginExecutionException("Target not set.");
+            }
+
+            var service = (IOrganizationService)serviceProvider.GetService(typeof(IOrganizationService));
+
+            // store pre/post entity images for later assertion
+            foreach (var preImage in context.PreEntityImages)
+            {
+                Entity preImageEntity = preImage.Value;
+                preImageEntity.Attributes.Remove("statecode");
+                bool imageContainedId = preImageEntity.Attributes.Remove(string.Format("{0}id", preImageEntity.LogicalName));
+                preImageEntity["preimagename"] = preImage.Key;
+                preImageEntity["containedid"] = imageContainedId;
+                preImageEntity["pluginstage"] = context.Stage;
+                preImageEntity.Id = Guid.NewGuid();
+
+                service.Create(preImageEntity);
+            }
+
+            foreach (var postImage in context.PostEntityImages)
+            {
+                Entity postImageEntity = postImage.Value;
+                postImageEntity.Attributes.Remove("statecode");
+                bool imageContainedId = postImageEntity.Attributes.Remove(string.Format("{0}id", postImageEntity.LogicalName));
+                postImageEntity["postimagename"] = postImage.Key;
+                postImageEntity["containedid"] = imageContainedId;
+                postImageEntity["pluginstage"] = context.Stage;
+                postImageEntity.Id = Guid.NewGuid();
+
+                service.Create(postImageEntity);
+            }
+        }
+    }
+}
